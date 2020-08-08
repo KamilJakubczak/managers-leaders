f9fba2eea2867226f3cba86f9dc6724c4f5bee64
noxiss <kamiljakubczak@outlook.com>
2020-08-08 16:05:55 +0200

    Added empty components for leads dashboard

---
+++ leadmanager/frontend/src/components/leads/Form.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Form extends Component {
+    render() {
+        return (
+            <div>
+           <h1>Leads Form to add</h1> 
+            </div>
+        )
+    }
+}
+
+export default Form
